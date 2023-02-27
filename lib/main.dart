4612ac9febe71c41f3f1a5e9187d8906d542b38a
aakash-301 <aakashshivanshu5@gmail.com>
2023-02-27 23:06:59 +0530

    small import fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo/common/utils/side_drawer.dart';
+import 'package:todo/common/side_drawer.dart';
 
 void main() {
   runApp(const MyApp());
