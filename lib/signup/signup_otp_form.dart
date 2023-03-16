e81fbf7df8f682eb973ac154f3c0f9737d237654
aakash-301 <aakashshivanshu5@gmail.com>
2023-03-16 13:17:12 +0530

    Sepeated Otp file and added validators in it

---
+++ lib/signup/signup_otp_form.dart
@@ -0,0 +1,140 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/otp_field_style.dart';
+import 'package:otp_text_field/style.dart';
+import 'package:todo/api/signup.dart';
+import 'package:todo/signup/signup_form_wrapper.dart';
+
+import '../constant/routes_table.dart';
+
+class SignupFormOtp extends StatefulWidget {
+  const SignupFormOtp({super.key});
+
+  @override
+  State<SignupFormOtp> createState() => _SignupFormOtpState();
+}
+
+class _SignupFormOtpState extends State<SignupFormOtp> {
+  bool _resendOtp = false;
+  bool _hasError = false;
+  String _otpFieldValue = "";
+
+  void _reSendOtp() {
+    if (_otpFieldValue.isEmpty || _otpFieldValue.length != 4) {
+      _hasError = true;
+      setState(() {});
+      return;
+    } else {
+      _hasError = false;
+      _resendOtp = true;
+    }
+    setState(() {});
+    SignUpApi.sendOtp();
+    return;
+  }
+
+  Future<void> _validateOtp() async {
+    if (_otpFieldValue.isEmpty || _otpFieldValue.length != 4) {
+      _hasError = true;
+      setState(() {});
+      return;
+    } else {
+      _hasError = false;
+    }
+    setState(() {});
+    Navigator.of(context).pushNamedAndRemoveUntil(
+      RouteTable.home,
+      (route) => false,
+    );
+    return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SignupFormWrapper(
+      hasAppbar: true,
+      child: Column(
+        children: [
+          const SizedBox(height: 30),
+          OTPTextField(
+            length: 4,
+            hasError: _hasError,
+            width: MediaQuery.of(context).size.width,
+            fieldWidth: 50,
+            keyboardType: TextInputType.number,
+            otpFieldStyle: OtpFieldStyle(
+              borderColor: Colors.black,
+              disabledBorderColor: Colors.grey,
+              errorBorderColor: Colors.red,
+            ),
+            style: const TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w600,
+            ),
+            textFieldAlignment: MainAxisAlignment.spaceAround,
+            fieldStyle: FieldStyle.underline,
+            onCompleted: (value) {
+              setState(() {
+                _otpFieldValue = value;
+                _hasError = false;
+              });
+            },
+          ),
+          const SizedBox(height: 20),
+          if (_hasError)
+            const Text(
+              "Enter correct OTP",
+              style: TextStyle(color: Colors.red),
+            ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Didn\'t recieved the code?',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                TextButton(
+                  onPressed: _reSendOtp,
+                  child: const Text(
+                    "Resend",
+                    style: TextStyle(fontSize: 16, color: Colors.blue),
+                  ),
+                ),
+                if (_resendOtp)
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text("00:${00}"),
+                  ),
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width - 150,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 1, 33, 59),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: TextButton(
+              onPressed: _validateOtp,
+              child: const Text(
+                "Validate",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
